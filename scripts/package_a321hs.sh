fd8460fb402b9f4635edce9cc5df32c71488dfa4
Robin Breitfeld <robin01196@gmail.com>
2023-08-03 11:04:17 +0200

    removed files which will get pulled from source

--- scripts/package_a321hs.sh
+++ scripts/package_a321hs.sh
@@ -3,8 +3,8 @@
 set -ex
 
 # copy from build into PackageSource
-cp -rva ./build-a321nneo/out/lvfr-horizonsim-airbus-a321-neo/. ./hsim-a321nneo/src/project/PackageSources
+cp -rva ./build-a321neo/out/lvfr-horizonsim-airbus-a321-neo/. ./hsim-a321neo/src/project/PackageSources
 
-cd /external/hsim-a321nneo/src/project/PackageSources
+cd /external/hsim-a321neo/src/project/PackageSources
 find . -type f -iname \*.PNG.DDS -delete
 find . -type f -iname \*.PNG.DDS.json -delete
