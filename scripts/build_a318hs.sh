d6a082bf70b942b25b8a82193e2f8ce461521d5c
Robin Breitfeld <robin01196@gmail.com>
2023-08-07 18:33:22 +0200

    fix script not to fail on rust

--- scripts/build_a318hs.sh
+++ scripts/build_a318hs.sh
@@ -9,15 +9,17 @@ ORIGINAL_GROUP_ID=$(stat -c '%g' /external)
 # set ownership to root to fix cargo/rust build (when run as github action)
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
   chown -R root:root /external
+  rm -rf /external/flybywire
 fi
 
 # permission fix
 git config --global --add safe.directory /external
+
 # run build
 time npx igniter -c a318ceo-igniter.config.mjs -r A318HS "$@"
 
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
-  rm -rf /external/flybywire
+  rm -rf /external/build-a318ceo/src
   rm -rf /external/hsim-a318ceo/src
 fi
 
--- scripts/build_a320hs.sh
+++ scripts/build_a320hs.sh
@@ -9,15 +9,17 @@ ORIGINAL_GROUP_ID=$(stat -c '%g' /external)
 # set ownership to root to fix cargo/rust build (when run as github action)
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
   chown -R root:root /external
+  rm -rf /external/flybywire
 fi
 
 # permission fix
 git config --global --add safe.directory /external
+
 # run build
 time npx igniter -c a320ceo-igniter.config.mjs -r A320HS "$@"
 
 if [ "${GITHUB_ACTIONS}" == "true" ]; then
-  rm -rf /external/flybywire
+  rm -rf /external/build-a320ceo/src
   rm -rf /external/hsim-a320ceo/src
 fi
 
