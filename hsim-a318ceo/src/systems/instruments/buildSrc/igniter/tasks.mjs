07eb9d86102cf3bdbf2b11dc380b2a41f6cd410c
Robin Breitfeld <robin01196@gmail.com>
2024-01-08 09:13:02 +0100

    [fbw] fix(build): nd dependencies (#8384)

--- hsim-a318ceo/src/systems/instruments/buildSrc/igniter/tasks.mjs
+++ hsim-a318ceo/src/systems/instruments/buildSrc/igniter/tasks.mjs
@@ -7,13 +7,17 @@ export function getInstrumentsIgniterTasks() {
     const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
         .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
 
-    return baseInstruments.map(({ name }) => new ExecTask(
-        name,
-        `cd build-a318ceo && mach build -f ${name}`,
-        [
-            join('build-a318ceo/src/systems/instruments/src', name),
-            'build-a318ceo/src/systems/instruments/src/Common',
-            join('build-a318ceo/out/lvfr-horizonsim-airbus-a318-ceo/html_ui/Pages/VCockpit/Instruments/A318HS', name),
-        ],
-    ));
+    return baseInstruments.map(({ name }) => {
+        const config = JSON.parse(fs.readFileSync(join(Directories.instruments, 'src', name, 'config.json')));
+        return new ExecTask(
+            name,
+            `cd build-a318ceo && mach build -f ${name}`,
+            [
+                join('build-a318ceo/src/systems/instruments/src', name),
+                'build-a318ceo/src/systems/instruments/src/Common',
+                join('build-a318ceo/out/lvfr-horizonsim-airbus-a318-ceo/html_ui/Pages/VCockpit/Instruments/A318HS', name),
+                ...(config.extraDeps || []),
+            ],
+        );
+    });
 }
--- hsim-a319ceo/src/systems/instruments/buildSrc/igniter/tasks.mjs
+++ hsim-a319ceo/src/systems/instruments/buildSrc/igniter/tasks.mjs
@@ -7,13 +7,17 @@ export function getInstrumentsIgniterTasks() {
     const baseInstruments = fs.readdirSync(join(Directories.instruments, 'src'), { withFileTypes: true })
         .filter((d) => d.isDirectory() && fs.existsSync(join(Directories.instruments, 'src', d.name, 'config.json')));
 
-    return baseInstruments.map(({ name }) => new ExecTask(
-        name,
-        `cd build-a319ceo && mach build -f ${name}`,
-        [
-            join('build-a319ceo/src/systems/instruments/src', name),
-            'build-a319ceo/src/systems/instruments/src/Common',
-            join('build-a319ceo/out/lvfr-horizonsim-airbus-a319-ceo/html_ui/Pages/VCockpit/Instruments/A319HS', name),
-        ],
-    ));
+    return baseInstruments.map(({ name }) => {
+        const config = JSON.parse(fs.readFileSync(join(Directories.instruments, 'src', name, 'config.json')));
+        return new ExecTask(
+            name,
+            `cd build-a319ceo && mach build -f ${name}`,
+            [
+                join('build-a319ceo/src/systems/instruments/src', name),
+                'build-a319ceo/src/systems/instruments/src/Common',
+                join('build-a319ceo/out/lvfr-horizonsim-airbus-a319-ceo/html_ui/Pages/VCockpit/Instruments/A319HS', name),
+                ...(config.extraDeps || []),
+            ],
+        );
+    });
 }
